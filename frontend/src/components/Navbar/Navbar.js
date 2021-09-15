2f10082b90d40f65660173c6968535568052a325
Patipan Buranangura <patipan.b7@gmail.com>
2021-09-16 00:04:33 +0700

    add Navbar

---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+      <div className="Navbar">
+        <h1 className="Header">Crypto Price</h1>
+      </div>
+    );
+}
+
+export default Navbar;
